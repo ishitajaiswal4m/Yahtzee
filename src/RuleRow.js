a42d52bcd2b50b38f13f5c4d504f115ef4215b7a
ishita <ishitajaiswal4m@gmail.com>
2020-09-29 21:07:29 +0530

    --updated: rules row

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,8 +3,11 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    const {name,score,doScore}=this.props;
+    const disabled = score !=undefined;
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr className={`RuleRow RuleRow-${disabled?"disabled":"active"}`} 
+          onClick={disabled? null : doScore}>
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
